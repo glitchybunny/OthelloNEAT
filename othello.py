10e0c24378715ae848261a05a70566270738aefc
Bunny <12066089+glitchybunny@users.noreply.github.com>
2022-01-21 00:51:55 +1100

    Fixed significant errors in the game logic, retrained

--- othello.py
+++ othello.py
@@ -20,34 +20,46 @@ START_BOARD = [
 OCT_DIRS = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
 WIN_BONUS = 64
 TOURNAMENT_FITNESS = 0
+random.seed()
 
 
 def possible_moves(_board, _turn):
     _pboard = _board.copy()
     valid_moves = []
-    for i in range(len(_pboard)):
+    for _i in range(len(_pboard)):
         # get current board pieces
-        if _pboard[i] == _turn:
-            pos = index_to_pos(i)
+        if _pboard[_i] == _turn:
             # walk in each oct direction to find possible moves
             for d in OCT_DIRS:
-                last = None
-                x = pos[0] + d[0]
-                y = pos[1] + d[1]
-                while last in [None, -_turn] and 0 <= x <= 7 and 0 <= y <= 7:
-                    index = pos_to_index((x, y))
-                    x += d[0]
-                    y += d[1]
-                    if _pboard[index] == 0 and last == -_turn:
-                        # update board with possible moves, and keep track of them separately in a list
-                        _pboard[index] = _turn << 1
-                        valid_moves.append(index)
-                    last = _pboard[index]
+                walk = True
+                can_play = False
+                pos = index_to_pos(_i)
+                pos[0] += d[0]
+                pos[1] += d[1]
+                while walk and 0 <= pos[0] <= 7 and 0 <= pos[1] <= 7:
+                    _j = pos_to_index(pos)
+                    if _pboard[_j] == -_turn:
+                        can_play = True
+                    elif _pboard[_j] == 0:
+                        if can_play:
+                            _pboard[_j] = 2 * _turn
+                            valid_moves.append(_j)
+                        walk = False
+                    else:
+                        walk = False
+                    pos[0] += d[0]
+                    pos[1] += d[1]
+
     return _pboard, valid_moves
 
 
 def perform_move(_board, _index, _turn):
+    if _board[_index] != 0:
+        raise RuntimeError("Can't place a tile in an already occupied space")
+
+    # place piece
     _board[_index] = _turn
+
     # walk in each oct direction to flank pieces
     _pos = index_to_pos(_index)
     for d in OCT_DIRS:
@@ -74,11 +86,11 @@ def perform_move(_board, _index, _turn):
 
 
 def index_to_pos(_index):
-    return int(_index % 8), int(_index // 8)
+    return [_index % 8, _index // 8]
 
 
 def pos_to_index(_pos):
-    return int(_pos[0] + _pos[1] * 8)
+    return _pos[0] + _pos[1] * 8
 
 
 def print_board(_board, _turn=None, _moves=None):
@@ -100,37 +112,25 @@ def print_board(_board, _turn=None, _moves=None):
 
 
 def eval_genome(genome, config):
-    # Set fitness to 0
-    genome.fitness = 0
+    # Generate network
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
 
-    # Fight agent against random number generator N times
-    count = 20
-    for _ in range(count//2):
+    # Fight agent against RNG players
+    fitness_sum = 0
+    count = 50
+    for _ in range(count // 2):
         # Make sure there's an equal number of games on each side
-        net = neat.nn.FeedForwardNetwork.create(genome, config)
         game_1 = eval_game(net, "random")
         game_2 = eval_game("random", net)
+        fitness_sum += game_1[0] + game_2[1]
 
-        # Get fitness
-        genome.fitness += game_1[0] + game_2[1]
+    # Return fitness
+    return fitness_sum
 
-    """
-    # Battle all agents against each other in the generation
-    for agents in list(combinations(genomes, 2)):
-        # Pick two agents at a time
-        alice_id, alice = agents[0]
-        a_net = neat.nn.FeedForwardNetwork.create(alice, config)
-        bob_id, bob = agents[1]
-        b_net = neat.nn.FeedForwardNetwork.create(bob, config)
 
-        # Fight them against each other twice, letting each side go first
-        game_1 = eval_game(a_net, b_net)
-        game_2 = eval_game(b_net, a_net)
-
-        # Sum their total fitness for all the games
-        alice.fitness += game_1[0] + game_2[1]
-        bob.fitness += game_1[1] + game_2[0]
-    """
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = eval_genome(genome, config)
 
 
 def eval_game(p1, p2):
@@ -181,12 +181,8 @@ def game_loop(p1, p2, board, turn):
             else:
                 # Parse output from neural network and make a move
                 output = p1.activate(pboard) if turn == BLACK else p2.activate(pboard)
-                candidate_max = 0
-                candidate_move = 0
-                for i in range(64):
-                    if i in moves and output[i] > candidate_max:
-                        candidate_max = output[i]
-                        candidate_move = i
+                output = [output[i]*(i in moves) for i in range(len(output))]
+                candidate_move = output.index(max(output))
             perform_move(board, candidate_move, turn)
             no_moves_available = False
         elif no_moves_available:
@@ -210,7 +206,7 @@ def run():
 
     # Create a population
     p = neat.Population(config)
-    # p = neat.Checkpointer.restore_checkpoint("checkpoints/alice/neat-checkpoint-25")
+    # p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-17")
 
     # Add reporter to show progress in the terminal
     p.add_reporter(neat.StdOutReporter(False))
@@ -219,8 +215,10 @@ def run():
     p.add_reporter(neat.Checkpointer(1, 5))
 
     # Train neural network
-    pe = neat.ParallelEvaluator(1 + multiprocessing.cpu_count(), eval_genome)
+    pe = neat.ParallelEvaluator(1 + multiprocessing.cpu_count(), eval_genome, timeout=60)
     winner = p.run(pe.evaluate, 50)
+    # winner = p.run(eval_genomes)
+
     print("Best fitness -> {}".format(winner))
 
     stats.save()
